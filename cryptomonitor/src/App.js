62d582a4f0976fdabaec40dd2cd0330db4e9a33c
Bruce van de Ven <430690@student.fontys.nl>
2021-03-16 16:03:15 +0100

    Keycloak geïmplementeerd

--- cryptomonitor/src/App.js
+++ cryptomonitor/src/App.js
@@ -1,12 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Welcome from './keycloak/Welcome';
+import Secured from './keycloak/Secured';
 import './App.css';
-import HomeScreen from './components/HomeScreen';
 
-function App() {
-  return (
-    <div className="App">
-      <HomeScreen />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <ul>
+            <li><Link to="/">public component</Link></li>
+            <li><Link to="/secured">secured component</Link></li>
+          </ul>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/secured" component={Secured} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
